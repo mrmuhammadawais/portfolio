09cec2e812e4a387b9ffd96ba5a1d60a65393b37
Muhammad Awais <111491333+mrmuhammadawais@users.noreply.github.com>
2024-05-17 01:06:36 +0500

    Banner.js

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -64,7 +64,6 @@ import TrackVisibility from 'react-on-screen';
                 {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
-                <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi I am Muhammad Awais`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer" ]'><span className="wrap">{text}</span></span></h1>
                   <p>For those who dream in code and design, welcome to my digital oasis. Here, lines of HTML, CSS, and JavaScript converge to create immersive online experiences that captivate and inspire. As a web developer, I am not merely a builder of websites; I am an architect of digital journeys. With a meticulous eye for detail and a passion for innovation, I specialize in crafting responsive, user-centric designs that blur the line between form and function. From sleek portfolios to dynamic e-commerce platforms, I bring ideas to life with elegance and efficiency. Join me as we embark on a voyage through the ever-evolving landscape of the web, where every project is an opportunity to push boundaries and redefine what's possible."</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
