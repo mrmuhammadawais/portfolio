2e9010da1aa330b25524c205da39deaa5ca11928
Muhammad Awais <111491333+mrmuhammadawais@users.noreply.github.com>
2024-05-18 20:21:05 +0500

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,4 @@
-
-import { useState,useRef} from "react";
+import { useState} from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import contactImg from "../../assets/img/contact-img.svg";
 import 'animate.css';
@@ -7,36 +6,15 @@ import TrackVisibility from 'react-on-screen';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
 
-export const Contact = () => {
-  const formInitialDetails = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
-  const [formDetails, setFormDetails] = useState(formInitialDetails);
-  const [buttonText] = useState('Send');
-  const [status] = useState({});
+export default function Contact() {
 
-  const onFormUpdate = (category, value) => {
-      setFormDetails({
-        ...formDetails,
-        [category]: value
-      })
-  }
-
-
-  const form = useRef();
-
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
+  function sendEmail(e) {
+      e.preventDefault();
+      emailjs
       .sendForm(
         "service_xcde6xg",
        'template_xsnk10l',
-        form.current, 
+       e.target, 
        "vUCuYt6JvPJgr-4Mj"
   )
       .then(
@@ -47,8 +25,50 @@ export const Contact = () => {
           console.log('FAILED...', error.text);
         },
       );
-    
+      e.target.reset(`Message Sent`);
+    }
+  
+  const formInitialDetails = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    message: ''
+  }
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState('Send');
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+      setFormDetails({
+        ...formDetails,
+        [category]: value
+      })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setButtonText("Sending...");
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(formDetails),
+    });
+    setButtonText("Send");
+    let result = await response.json();
+    setFormDetails(formInitialDetails);
+    if (result.code ===200) {
+      setStatus({ succes: true, message: 'Message sent successfully'});
+      console.log('Message Sent');
+    } else {
+      setStatus({ success: false, message: 'Something went wrong, please try again later.'});
+    }
   };
+ 
+
+  
   return (
     <section className="contact" id="connect">
       <Container>
@@ -65,26 +85,26 @@ export const Contact = () => {
               {({ isVisible }) =>
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <h2>Get In Touch</h2>
-                <form ref={form} onSubmit={sendEmail}>
+                <form onSubmit={sendEmail}>
               
                   <Row>
-                
+                 
      <Col size={12} sm={6} className="px-1">
-         <input type="text" name="user_name" placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
-                    </Col>
-                    <Col size={12} sm={6} className="px-1">
+         <input type="text" name="user_name" placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} /> 
+                     </Col>
+                    <Col size={12} sm={6} className="px-1"> 
                     <input type="text" name="user_name" placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                    </Col>
+                   </Col> 
                     <Col size={12} sm={6} className="px-1">
                     <input type="email" name="user_email" placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
                     </Col>
                     <Col size={12} sm={6} className="px-1">
-                      <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
+                    <input type="text" name="user_name" placeholder="Phone Number" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
                     </Col>
                     <Col size={12} className="px-1">
                     <textarea  rows={6} className="px-1" name="message"  placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>{buttonText}</span></button>
-                    </Col>
+                    </Col> 
      
                     {
                       status.message &&
@@ -101,5 +121,6 @@ export const Contact = () => {
       </Container>
     </section>
   )
+
 }
 
