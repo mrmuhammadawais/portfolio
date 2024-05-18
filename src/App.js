e27dd3aa66e83a617be33063f217d5edb061a441
Muhammad Awais <111491333+mrmuhammadawais@users.noreply.github.com>
2024-05-18 20:23:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {NavBar} from './components/Navbar/NavBar';
 import {Banner} from './components/Header/Banner';
 import {Skills} from './components/Skills/Skills';
 import {Projects} from './components/Projects/Projects';
-import { Contact } from './components/Contact/Contact';
+import  Contact  from './components/Contact/Contact';
 import { Footer } from './components/Footer/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
